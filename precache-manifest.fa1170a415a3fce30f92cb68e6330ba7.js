be22f64effed59050501f957cd7f12a4dc6e1d19
Jinjia Lin <linjj93@hotmail.com>
2018-11-22 23:01:26 +0800

    Updates

---
+++ precache-manifest.fa1170a415a3fce30f92cb68e6330ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86a1c5fc896e24392f87",
+    "url": "/backpackingtrip/static/css/main.a9866cff.chunk.css"
+  },
+  {
+    "revision": "86a1c5fc896e24392f87",
+    "url": "/backpackingtrip/static/js/main.86a1c5fc.chunk.js"
+  },
+  {
+    "revision": "ee84d47c6784ebeca86f",
+    "url": "/backpackingtrip/static/js/1.ee84d47c.chunk.js"
+  },
+  {
+    "revision": "48d14a0cc7fc8424a35a",
+    "url": "/backpackingtrip/static/js/runtime~main.48d14a0c.js"
+  },
+  {
+    "revision": "305b5a180710e83918f40a1d76a06b38",
+    "url": "/backpackingtrip/static/media/bags.305b5a18.jpg"
+  },
+  {
+    "revision": "3be7e4fb1bf3754c2891aa91244263d4",
+    "url": "/backpackingtrip/index.html"
+  }
+];
